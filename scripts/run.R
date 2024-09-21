aca0889b10333d065db82dbcca12949de5bdb5be
almac2022 <al@newgraphenvironment.com>
2024-09-21 07:17:50 -0700

    move some functions to staticimports system

--- scripts/run.R
+++ scripts/run.R
@@ -4,9 +4,12 @@ preview_chapter('0100-intro.Rmd')
 #################################################################################################
 ##go to the index.Rmd and change gitbook_on <- TRUE
 #################################################################################################
+
+
 {
-  source("scripts/functions.R")
-  news_to_appendix()
+  staticimports::import()
+  source('scripts/staticimports.R')
+  my_news_to_appendix()
 
   rmarkdown::render_site(output_format = 'bookdown::gitbook',
                          encoding = 'UTF-8')
---
+++ scripts/utils.R
@@ -0,0 +1,5 @@
+# @staticimports pkg:staticimports
+#  my_news_to_appendix
+#  my_dt_table
+#  my_tab_caption
+#  my_leaflet
