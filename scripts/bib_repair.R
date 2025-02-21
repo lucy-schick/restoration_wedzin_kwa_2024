926bd7bd0c34889c61e40bb0af1e4afea5439bc8
almac2022 <al@newgraphenvironment.com>
2025-02-20 19:44:35 -0800

    imporove documentation

--- scripts/bib_repair.R
+++ scripts/bib_repair.R
@@ -12,13 +12,13 @@ keys = rbbt::bbt_detect_citations(
 path_bib <- system.file("extdata", "NewGraphEnvironment.bib", package = "xciter")
 
 
-keys_missing <- xciter::xct_bib_keys_missing(
-  path_bib = path_bib,
-  citations = rbbt::bbt_detect_citations(
-    list.files(
-      pattern = "*.Rmd")
-  )
-)
+# keys_missing <- xciter::xct_bib_keys_missing(
+#   path_bib = path_bib,
+#   citations = rbbt::bbt_detect_citations(
+#     list.files(
+#       pattern = "*.Rmd")
+#   )
+# )
 
 # print to console so we can copy paste into xciter data
 # cat("keys_missing <- c(\n",
@@ -38,9 +38,18 @@ file_list <- fs::dir_ls(glob = "*.Rmd")
 
 # replace them
 purrr::map2(
-  .x = xct_xref_citations_match$key_match,
+  .x = xct_xref_citations_match$key_missing,
   .y = xct_xref_citations_match$key_match,
   ~ ngr::ngr_str_replace_in_files(text_current = .x, text_replace = .y, files = file_list)
+)
+
+# now we can check for missing keys with more likelyhood there will not be incorrect matches.
+keys_missing <- xciter::xct_bib_keys_missing(
+  path_bib = path_bib,
+  citations = rbbt::bbt_detect_citations(
+    list.files(
+      pattern = "*.Rmd")
+  )
 )
 
 
@@ -52,16 +61,15 @@ keys_matched <- xciter::xct_keys_guess_match(
   no_match_rows_include = TRUE
 ) |>
   dplyr::arrange(key_missing)
-  # dplyr::filter(!is.na(key_missing_guess_match))
+
+# we check the matches to be 100% sure they are air tight (hopefully)
 
 
-
-
-
+# they look good so we replace all at once without asking
 purrr::map2(
   .x = keys_matched$key_missing,
   .y = keys_matched$key_missing_guess_match,
-  ~ ngr::ngr_str_replace_in_files(text_current = .x, text_replace = .y, files = file_list)
+  ~ ngr::ngr_str_replace_in_files(text_current = .x, text_replace = .y, files = file_list, ask = FALSE)
 )
 
 
